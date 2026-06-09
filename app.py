2fefa12e676f348caf11e4ebf37fe3abb223f88b
Victor-prog-alt <v.stanilov010@gmail.com>
2026-06-09 12:37:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,51 @@
+import streamlit as st
+import easyocr
+import numpy as np
+from PIL import Image
+import cv2
+import re
+from rapidfuzz import fuzz
 
+========================================
+PAGE CONFIG
+========================================
+
+st.set_page_config(
+    page_title="AI Ingredient Scanner",
+    page_icon="🧪",
+    layout="centered"
+)
+
+========================================
+LOAD OCR
+========================================
+
+@st.cache_resource
+def load_reader():
+    return easyocr.Reader(['bg', 'en'])
+
+reader = load_reader()
+
+========================================
+DATABASE
+========================================
+
+INGREDIENT_DATABASE = {
+
+    # SWEETENERS
+
+    "E950": {
+        "en": "Acesulfame K",
+        "bg": "Ацесулфам К",
+        "risk": 3,
+        "category": "Sweetener",
+        "info": "Artificial sweetener",
+        "aliases": [
+            "e950",
+            "acesulfame k",
+            "ацесулфам",
+            "ацесулфам к"
+        ]
+    },
+
+    "E951
